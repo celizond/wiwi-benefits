ca0264e4b3ef9d534dc98a16dd29abb70d4fd718
celizond <elizondo.celular@gmail.com>
2025-03-27 10:38:03 -0300

    create benefit in db

--- src/benefits/benefits.service.ts
+++ src/benefits/benefits.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBenefitDto } from './dto/create-benefit.dto';
 import { UpdateBenefitDto } from './dto/update-benefit.dto';
+import { Benefit } from './entities/benefit.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class BenefitsService {
-  create(createBenefitDto: CreateBenefitDto) {
-    return 'This action adds a new benefit';
-  }
+
+  constructor (
+    @InjectModel(Benefit.name) //Nombre de modelo
+    private readonly benefitModel: Model<Benefit> 
+  ) {}
+
+  async create(createBenefitDto: CreateBenefitDto) {
+    //createBenefitDto.commerce = createBenefitDto.commerce.toLowerCase();
+      try {
+        const benefit = await this.benefitModel.create(createBenefitDto);
+        return benefit;
+      } catch (error) {
+        //this.handleExceptions(error);
+      }
+    }
 
   findAll() {
     return `This action returns all benefits`;
